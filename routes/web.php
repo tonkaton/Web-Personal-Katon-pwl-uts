5d2643a45d7950e888c78bbc70d5331da21405b6
tonkaton <katonkurnia28@gmail.com>
2023-11-21 19:30:52 +0700

    ada perubahan kecil di WebKaton

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\portofolioController;
+use App\Http\Controllers\PortofolioController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\AuthController;
